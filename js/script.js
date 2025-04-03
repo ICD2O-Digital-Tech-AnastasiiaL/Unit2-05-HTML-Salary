043d605f6feeed5fc969c4f7e06a2e5ec3c25727
Anastasiia Lebedenko <anastasiia.lebedenko@stu.ocsb.ca>
2025-04-03 14:56:11 +0000

    salary calculator

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ms Raffin
+// Created on: Sep 2022
 // This file contains the JS functions for index.html
+"use strict";
+
+// This function calculates the gross pay, government take, and net pay
+function hoursWorked() {
+  // input
+  let hoursWorked = parseFloat(document.getElementById("hours-worked").value);
+  let hourlyRate = parseFloat(document.getElementById("hourly-rate").value);
+    const TAX = 20.05;
+  // process
+  let grossPay = hoursWorked * hourlyRate;
+  let govTake = grossPay * (TAX / 100);
+  let netPay = grossPay - govTake;
+  // output
+  document.getElementById("gross-pay").innerHTML = "Gross Pay: $" + grossPay.toFixed(2);
+  document.getElementById("gov-take").innerHTML = "Government Take: $" + govTake.toFixed(2);
+  document.getElementById("net-pay").innerHTML = "Net Pay: $" + netPay.toFixed(2);
+} 
